e510bd9be6b13e1ed7c7df8128b19a7b2cba6a57
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-03-04 14:21:12 +0700

    two commit 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -4,39 +4,33 @@ function getResult(a,b,c){
     let x = [];
     let D = b**2 - 4 * a * c;
 
-    if (D < 0) {
-      return x;
-    } else if (D == 0) {
+    if (D == 0) {
       x.push((-b + Math.sqrt(D)) / (2 * a));
-      return x;
     } else if (D > 0) {
       x.push((-b + Math.sqrt(D)) / (2 * a));
       x.push((-b - Math.sqrt(D)) / (2 * a));
-      return x;
     }
+    return x;
     // код для задачи №1 писать здесь
-    // return x;
 }
 
+
 function getAverageMark(marks){
-    let marks = [];
+  
     let sum = 0;
 
     if (marks.length === 0) {
         return 0;
     } else if (marks.length > 5) {
-        marks.slice(0, 5);
-        console.log("Вы хотите вывести больше оценок?");
+        marks.splice(0, 5);
     }
 
-    for (let i = 0; i = marks.length; i++) {
+    for (let i = 0; i < marks.length; i++) {
         sum += marks[i];
     }
     console.log(sum);
 
-    let getAverageMark = sum / marks.length;
-
-    return averageMark;
+    return sum / marks.length;
     // код для задачи №2 писать здесь
     // return averageMark;
 }
