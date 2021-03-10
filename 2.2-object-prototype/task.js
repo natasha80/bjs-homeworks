365370e071b040eb862699266bd866472d8401a4
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-03-10 17:51:47 +0700

    first commit 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,6 +1,28 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict";
+
+String.prototype.isPalindrome = function isPalindrome() {
+    let string = this.toLowerCase().replace(/\s/g, "").split("");
+    console.log(string.join());
+    console.log(string.reverse().join());
+    return string === string.reverse();
+}
+    //String.prototype.isPalindrome - для задачи №1
+    
 
 function getAverageMark(marks) {
+    let sum = 0;
+
+    if (marks.length === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    const average = sum / marks.length;
+    const roundedAverage = Math.round(average);
+
+    return roundedAverage;
     // код для задачи №2 писать здесь
     // return averageMark
 }
