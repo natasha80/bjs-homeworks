491a915bb5ed2e6cb0a94c79a38a1afb3b98fc3a
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-03-11 21:27:35 +0700

    исправления 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,8 +1,30 @@
 "use strict";
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    let S = 50000 - 0;
-    let P = 10 / 100 / 12;
+     
+    if (percent >= 0) {  
+      percent = +percent;
+    } else if (percent != 0 || percent == 0) {
+      return (`Параметр "Процентная ставка" содержит неправильное значение ${percent}`);
+    }
+    if (contribution > 0) {  
+      сontribution = +contribution;
+    } else if (contribution != 0) {
+      return (`Параметр "Начальный взнос" содержит неправильное значение ${contribution}`); 
+    }
+          
+    if (amount >= 0) {  
+      amount = +amount;
+    } else if (amount !=  0) {
+      return (`Параметр "Общая стоимость" содержит неправильное значение ${amount}`); 
+    }
+
+    if (date >= date) {
+      return (`Параметр "Срок ипотеки" содержит неправильное значение ${date}`); 
+    } 
+
+    let S =  amount - contribution;
+    let P = percent / 100 / 12;
     let n = 12;
 
     let monthlyFee = S * (P + P / (((1 + P) ** n) - 1));
@@ -10,19 +32,21 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let totalAmount = 0 + (monthlyFee * n);
 
     console.log(totalAmount.toFixed(2));
-    // код для задачи №1 писать здесь
-    return totalAmount;
+    return totalAmount.toFixed(2);
 }
 
 
 function getGreeting(name) {
+    let greeting = "";
     if (name === undefined || name === null || name === "") {
-        console.log ("Привет, мир! Меня зовут Аноним");
-        return "Привет, мир! Меня зовут Аноним";
+        greeting = (`Привет, мир! Меня зовут Аноним`);
+        console.log(greeting);
+        return greeting;
     } else {
-        console.log (`Привет, мир! Меня зовут ${name}`);
-        return `Привет, мир! Меня зовут ${name}`;
+        greeting = (`Привет, мир! Меня зовут ${name}`);
+        console.log(greeting); 
+        return greeting;
     }
     // код для задачи №2 писать здесь
-    return greeting;
+    // return greeting;
 }
