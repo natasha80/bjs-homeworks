0c919617a5f57a611a20834e96a7b0fec0617a5a
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-03-13 18:34:05 +0700

    исправление 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,51 +2,35 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
      
-    if (percent >= 0) {  
-      percent = +percent;
-    } else if (percent != 0 || percent == 0) {
+    if (+percent >= 0 || +percent == 0){  
       return (`Параметр "Процентная ставка" содержит неправильное значение ${percent}`);
     }
-    if (contribution > 0) {  
-      сontribution = +contribution;
-    } else if (contribution != 0) {
-      return (`Параметр "Начальный взнос" содержит неправильное значение ${contribution}`); 
+
+    if (+contribution > 0 || +contribution == 0) {
+      return (`Параметр "Начальный взнос" содержит неправильное значение ${contribution}`);
     }
+    
           
-    if (amount >= 0) {  
-      amount = +amount;
-    } else if (amount !=  0) {
-      return (`Параметр "Общая стоимость" содержит неправильное значение ${amount}`); 
+    if (+amount >= 0 || +amount == 0) {
+      return (`Параметр "Общая стоимость" содержит неправильное значение ${amount}`);
     }
 
-    if (date >= date) {
+    let S = parseInt(amount) - parseInt(contribution);
+    let P = percent / 12 / 100;
+    let n = (newDate() - +(date).newDate()) / 30;
+
+    if (date <= 0) {
       return (`Параметр "Срок ипотеки" содержит неправильное значение ${date}`); 
     } 
 
-    let S =  amount - contribution;
-    let P = percent / 100 / 12;
-    let n = 12;
-
     let monthlyFee = S * (P + P / (((1 + P) ** n) - 1));
 
-    let totalAmount = 0 + (monthlyFee * n);
+    let totalAmount = (monthlyFee * n);
 
-    console.log(totalAmount.toFixed(2));
     return totalAmount.toFixed(2);
 }
 
 
 function getGreeting(name) {
-    let greeting = "";
-    if (name === undefined || name === null || name === "") {
-        greeting = (`Привет, мир! Меня зовут Аноним`);
-        console.log(greeting);
-        return greeting;
-    } else {
-        greeting = (`Привет, мир! Меня зовут ${name}`);
-        console.log(greeting); 
-        return greeting;
-    }
-    // код для задачи №2 писать здесь
-    // return greeting;
+  return `Привет, мир! Меня зовут ${name || "Аноним"}.`;
 }
