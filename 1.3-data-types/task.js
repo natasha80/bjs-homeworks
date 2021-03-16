13c32a186d9098c8aeae3d9894c30a8e04a93ff6
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-03-16 19:05:06 +0700

    исправление 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,24 +2,25 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
      
-    if (+percent >= 0 || +percent == 0){  
+    if (+percent <= isNaN(percent) || typeof +percent === "undefined") { 
+      console.log(percent ) ;
       return (`Параметр "Процентная ставка" содержит неправильное значение ${percent}`);
     }
 
-    if (+contribution > 0 || +contribution == 0) {
+    if (+contribution < contribution || typeof +contribution === "undefined") {
       return (`Параметр "Начальный взнос" содержит неправильное значение ${contribution}`);
     }
     
-          
-    if (+amount >= 0 || +amount == 0) {
+    if (+amount <= isNaN(amount) || typeof +amount === "undefined") {
       return (`Параметр "Общая стоимость" содержит неправильное значение ${amount}`);
     }
 
     let S = parseInt(amount) - parseInt(contribution);
-    let P = percent / 12 / 100;
-    let n = (newDate() - +(date).newDate()) / 30;
+    let P = percent / 100 / 12;
+    let day = new Date().getFullYear;
+    let n = new Date().getFullYear() / 30;
 
-    if (date <= 0) {
+    if (date <= isNaN(date)) {
       return (`Параметр "Срок ипотеки" содержит неправильное значение ${date}`); 
     } 
 
@@ -27,7 +28,9 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
     let totalAmount = (monthlyFee * n);
 
+    console.log(date);
     return totalAmount.toFixed(2);
+    
 }
 
 
