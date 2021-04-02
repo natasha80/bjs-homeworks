7887d7ab062adef8434b113fc2739efd80f97177
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-04-02 18:24:52 +0700

    исправление 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,39 +1,34 @@
 "use strict";
 
+// Задача 1
 function calculateTotalMortgage(percent, contribution, amount, date) {
-     
-    if (+percent <= isNaN(percent) || typeof +percent === "undefined") { 
-      console.log(percent ) ;
-      return (`Параметр "Процентная ставка" содержит неправильное значение ${percent}`);
-    }
+  if (percent <= 0 || typeof(percent) === "string") {
+  return `Параметр "Процентная ставка" содержит неправильное значение ${percent}.`;
+  }// Проверяем на корректность
+  
+  if (contribution > amount || typeof(contribution) === "string") {
+  return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}.`;
+  }// Проверяем на корректность
+  
+  if (amount < contribution || typeof(amount) === "string") {
+  return `Параметр "Общая стоимость" содержит неправильное значение ${amount}.`;
+  }// Проверяем на корректность
+  //S - сумма кредита, P - 1/12 процентной ставки, n - кол-во месяцев ^-возведение в степень
+  let S = parseInt(amount) - parseInt(contribution);//Сумма которую нужно вернуть банку(Сумма кредита - первый взнос)
+  let P = percent / 100 / 12;//Проценты
+  let yearsDiff = date.getFullYear() - new Date().getFullYear();
+  let n = date.getMonth() - new Date().getMonth() + yearsDiff * 12;//Кол-во месяцев ипотеки
 
-    if (+contribution < contribution || typeof +contribution === "undefined") {
-      return (`Параметр "Начальный взнос" содержит неправильное значение ${contribution}`);
-    }
-    
-    if (+amount <= isNaN(amount) || typeof +amount === "undefined") {
-      return (`Параметр "Общая стоимость" содержит неправильное значение ${amount}`);
-    }
-
-    let S = parseInt(amount) - parseInt(contribution);
-    let P = percent / 100 / 12;
-    let day = new Date().getFullYear;
-    let n = new Date().getFullYear() / 30;
-
-    if (date <= isNaN(date)) {
-      return (`Параметр "Срок ипотеки" содержит неправильное значение ${date}`); 
-    } 
-
-    let monthlyFee = S * (P + P / (((1 + P) ** n) - 1));
-
-    let totalAmount = (monthlyFee * n);
-
-    console.log(date);
-    return totalAmount.toFixed(2);
-    
+  let monthlyFee = S * (P + P / (((1 + P) ** n) - 1));//Ежемесячный платеж
+  let totalAmount = (monthlyFee * n);//Сумма которую должен заплатить клиент
+  let result = Math.round(totalAmount * 100) / 100;
+  console.log(result);
+  return result;
 }
 
-
+// Задача 2
 function getGreeting(name) {
   return `Привет, мир! Меня зовут ${name || "Аноним"}.`;
 }
+
+
