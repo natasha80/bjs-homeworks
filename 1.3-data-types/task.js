08dd4625b215e8404912f6a78aa427e2efaeb467
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-04-07 07:41:17 +0700

    исправления 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,15 +2,15 @@
 
 // Задача 1
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  if (percent <= 0 || typeof(percent) === "string") {
+  if (isNaN(percent) || percent <= 0) {
   return `Параметр "Процентная ставка" содержит неправильное значение ${percent}.`;
   }// Проверяем на корректность
   
-  if (contribution > amount || typeof(contribution) === "string") {
+  if (isNaN(contribution) || contribution > amount) {
   return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}.`;
   }// Проверяем на корректность
   
-  if (amount < contribution || typeof(amount) === "string") {
+  if (isNaN(amount) || amount < contribution) {
   return `Параметр "Общая стоимость" содержит неправильное значение ${amount}.`;
   }// Проверяем на корректность
   //S - сумма кредита, P - 1/12 процентной ставки, n - кол-во месяцев ^-возведение в степень
@@ -22,9 +22,11 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let monthlyFee = S * (P + P / (((1 + P) ** n) - 1));//Ежемесячный платеж
   let totalAmount = (monthlyFee * n);//Сумма которую должен заплатить клиент
   let result = Math.round(totalAmount * 100) / 100;
+
   console.log(result);
   return result;
 }
+
 
 // Задача 2
 function getGreeting(name) {
